b60ae102e810d8ad725a6bbc13d1c22927fbc727
Cláudio Barroso <109551603+aubaro01@users.noreply.github.com>
2025-04-10 10:05:51 +0100

    fix

--- src/pages/pageMain.js
+++ src/pages/pageMain.js
@@ -25,21 +25,12 @@ export default function LandingPage() {
 
     setLoading(true);
     setErrorMessage(""); 
-<<<<<<< Updated upstream
-=======
     
 try {
   const response = await axios.post(`${process.env.REACT_APP_API_URL}/api/users`, formData, {
     headers: { "Content-Type": "application/json" },
-  });
->>>>>>> Stashed changes
-
-    try{
-    const response = await axios.post("###",formData,
-      {
-        headers: { "Content-Type": "application/json" },
-      }
-    );
+});
+    t
   if (response.status === 200  || response.status === 201) {
     alert("Inscrição enviada com sucesso!");
   } else {
